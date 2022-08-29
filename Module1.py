49509f07e330a3ae53e06c7f3ecd0dd0cf2ee5d4
Mayur Kadam <mayurk98194@gmail.com>
2022-08-29 16:13:43 +0530

    Add files via upload

---
+++ Module1.py
@@ -0,0 +1,7 @@
+#importing specific class from library
+from datetime import date
+print(date.today())
+
+#use of __name__ in python
+print("name of this module is ",__name__)
+#By default it is main if we run directly basically
---
+++ Module2.py
@@ -0,0 +1,4 @@
+import Module1
+print("in the second file the module name is ",__name__)
+#since over here it is executed directly hence the variable becomes main
+#whereas the older one becomes the object name
